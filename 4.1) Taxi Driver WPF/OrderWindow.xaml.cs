5b1332d05dfc95f1415cddc3f5a7bd3b3704025c
BohdanRomaniuk <bohdan2307@gmail.com>
2017-10-18 11:17:30 +0300

    Add timer and cost in order window

--- 4.1) Taxi Driver WPF/OrderWindow.xaml.cs
+++ 4.1) Taxi Driver WPF/OrderWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using Taxi_Driver_WPF.DataTypes;
+using System.Windows.Threading;
 
 namespace _4._1__Taxi_Driver_WPF
 {
@@ -20,23 +21,43 @@ namespace _4._1__Taxi_Driver_WPF
 	public partial class OrderWindow : Window
 	{
 		private TaxiOrder currentOrder;
-		public OrderWindow(TaxiOrder _currentOrder)
+		DispatcherTimer dispatcherTimer = new DispatcherTimer();
+		DateTime startTime;
+		TimeSpan elapsedTime;
+        public OrderWindow(TaxiOrder _currentOrder)
 		{
 			InitializeComponent();
 			currentOrder = _currentOrder;
+			
 			clientNameDesc.Content = currentOrder.Client.Name;
 			clientPhoneDesc.Content = currentOrder.Client.PhoneNumber;
 			clientFromDesc.Content = currentOrder.Dispatch;
 			clientToDesc.Content = currentOrder.Destination;
 			clientTimeDesc.Content = currentOrder.ArriveTime;
-        }
+		}
+		private void startTimer()
+		{
+			dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+			dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+			dispatcherTimer.Start();
+			startTime = DateTime.Now;
+		}
+		private void dispatcherTimer_Tick(object sender, EventArgs e)
+		{
+			elapsedTime = DateTime.Now - startTime;
+			timerDesc.Content = elapsedTime.ToString();
+		}
 		private void startRoad_Click(object sender, RoutedEventArgs e)
 		{
-
+			startTimer();
 		}
 		private void endRoad_Click(object sender, RoutedEventArgs e)
 		{
-
-		}
+			dispatcherTimer.Stop();
+            currentOrder.RoadTime = (uint)elapsedTime.TotalSeconds;
+			currentOrder.IsDone = true;
+			currentOrder.Cost = currentOrder.Driver.CostPerMinute * currentOrder.RoadTime;
+			roadCostDesc.Content = currentOrder.Cost + " грн";
+        }
 	}
 }
