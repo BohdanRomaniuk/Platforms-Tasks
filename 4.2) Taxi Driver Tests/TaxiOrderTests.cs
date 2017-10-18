56a262eea211ed1931ef721e2777c26d896cc6ef
BohdanRomaniuk <bohdan2307@gmail.com>
2017-10-18 22:13:00 +0300

    Update tests

--- 4.2) Taxi Driver Tests/TaxiOrderTests.cs
+++ 4.2) Taxi Driver Tests/TaxiOrderTests.cs
@@ -10,23 +10,25 @@ namespace Taxi_Tests
 		[TestMethod]
 		public void Test_TaxiOrderConstructorAndProperties()
 		{
-			TaxiDriver driver = new TaxiDriver(1, "Радомський", "Модест", 20, "BC1789AM", 5, 50, 189.75);
-			Assert.AreEqual(driver.Surname, "Радомський");
-			Assert.AreEqual(driver.Name, "Модест");
-			Assert.AreEqual(driver.Age, Convert.ToUInt32(20));
-			Assert.AreEqual(driver.CarNumber, "BC1789AM");
-			Assert.AreEqual(driver.Experience, Convert.ToUInt32(5));
-			Assert.AreEqual(driver.CostPerMinute, Convert.ToUInt32(50));
-			Assert.AreEqual(driver.PayCheck, 189.75);
+			TaxiClient client = new TaxiClient(2, "Модест", "+380966784576");
+			TaxiDriver driver = new TaxiDriver(3, "Радомський", "Модест", 20, "BC1789AM", 5, 50, 189.75);
+			TaxiOrder order = new TaxiOrder(1, client, driver, Convert.ToDateTime("2017-12-22 17:30"), "Шевченка,44", "Зелена,12", 123);
+			Assert.AreEqual(order.Client.Id, client.Id);
+			Assert.AreEqual(order.Driver.Id, driver.Id);
+			Assert.AreEqual(order.ArriveTime, Convert.ToDateTime("2017-12-22 17:30"));
+			Assert.AreEqual(order.Dispatch, "Шевченка,44");
+			Assert.AreEqual(order.Destination, "Зелена,12");
+			Assert.AreEqual(order.RoadTime, Convert.ToUInt32(123));
 		}
 
 		[TestMethod]
 		[ExpectedException(typeof(ArgumentOutOfRangeException))]
 		public void Test_TaxiOrderPopertiesException()
 		{
-			TaxiDriver driver = new TaxiDriver(1, "Радомський", "Модест", 20, "BC1789AM", 5, 50, 189);
-			//not valid age
-			driver.Age = 17;
+			TaxiOrder order = new TaxiOrder();
+			order.Dispatch = "Виговського,123";
+			//Destination equals to Dispatch
+			order.Destination = "Виговського,123";
 		}
 	}
 }
