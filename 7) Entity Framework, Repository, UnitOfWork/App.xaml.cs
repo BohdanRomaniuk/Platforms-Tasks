bec1bcd6121cec297af7931956081635b396e82f
BohdanRomaniuk <bohdan2307@gmail.com>
2017-11-15 00:08:14 +0200

    Add last project design

---
+++ 7) Entity Framework, Repository, UnitOfWork/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _7__Entity_Framework__Repository__UnitOfWork
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
