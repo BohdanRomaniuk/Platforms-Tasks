0cee047f0d533fc8936a010d3d5300580b6b81c3
BohdanRomaniuk <bohdan2307@gmail.com>
2017-11-14 21:50:00 +0200

    Add Ado.Net Project Implementation

--- 6) ADO.NET/ADO.NET.cs
+++ 6) ADO.NET/ADO.NET.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
-namespace _6__ADO.NET
+namespace ADO.NET
 {
-    class Program
+    class NorthWindTasks
     {
         static void Main(string[] args)
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["ADO.NET.Properties.Settings.NorthwindConnectionString"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            Console.WriteLine("Show the list of first and last names of the employees from London");
+            SqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT FirstName, LastName FROM Employees WHERE City='London';";
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0}\t{1}", reader["FirstName"], reader["LastName"]);
+            }
+            reader.Close();
+
+            Console.WriteLine("\nCalculate the count of employees from London");
+            command.CommandText = "SELECT COUNT(*) AS EmployeeQuantity FROM Employees WHERE City='London';";
+            reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader["EmployeeQuantity"]);
+            }
+            reader.Close();
+
+
+
+            connection.Close();
+            Console.ReadKey();
         }
     }
 }
